bdb52c6e2eb5d7a52d42c7fccc45d45aa53c3566
Paultree <paulphamily@gmail.com>
2022-08-05 15:49:15 +1000

    basic skeleton implementation

---
+++ script.js
@@ -0,0 +1,25 @@
+// math operators //
+function add(a,b) {
+    let result = a + b;
+    return result;
+}
+
+function subtract(a,b) {
+    let result = a - b;
+    return result;
+}
+
+function multiply(a,b) {
+    let result = a * b;
+    return result;
+}
+
+function divide(a,b) {
+    let result = a/b;
+    return result;
+}
+
+// simple calculation function //
+function operate(a,b,operator) {
+    return operator(a,b);
+}
