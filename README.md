d338758b90bb6f6c093bf8d8a3c917830da87818
Paultree <paulphamily@gmail.com>
2022-08-06 17:42:17 +1000

    simple calculator v1

--- README.md
+++ README.md
@@ -14,3 +14,10 @@
 - Bugs to be fixed: 'undefined appearing on the calculator display if div container 'numbers' is clicked or event triggered.
 - TBD: implement a feature where display content font size changes as more numbers are added. alternatively, we can try add a feature that allows the numbers to continue off but once it clips the display border, the number hides and only the most recent 10 or so numbers (maximum limit of digits allowed on display) to show.
 - TBD: Be able to do a simple calculation.
+
+06/08/2022
+- Implemented simple calculation functionality.
+- TBD:
+    - Your calculator should not evaluate more than a single pair of numbers at a time. If we are doing a multi operator calculation, it should show result from first operation and then follow-up calculation from 2nd operation and so on and so forth.
+    - Be able to do decimal calculation.
+    - Put an error message when try to divide by 0.
--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // math operators //
 function add(a,b) {
-    return a += b;
+    result += a + b;
+    return result;
 }
 
 function subtract(a,b) {
-    return a -= b;
+    result += a - b;
+    return result;
 }
 
 function multiply(a,b) {
-    return a *=b;
+    result += a * b;
+    return result;
 }
 
 function divide(a,b) {
-    return a /=b;
+    result += a/b;
+    return result;
 }
 
 // simple calculation function //
-function operate(a,b,operator) {
+function operate(a, operator, b) {
     return operator(a,b);
 }
+
+let result = 0;
 
 const numArr = {
     zero: 0,
@@ -31,6 +37,7 @@ const numArr = {
     seven: 7,
     eight: 8,
     nine: 9,
+    decimal: '.',
 }
 
 // Simple number pressing function //
@@ -38,7 +45,70 @@ const numbers = document.querySelector('#numbers');
 const display = document.querySelector('.calculator-display');
 
 numbers.addEventListener('click', (e) => {
-    display.textContent += numArr[`${e.target.id}`]
-   })
+    if (e.target.id == 'numbers' || e.target.id == 'clear') {
+        return;
+    } else {
+        display.textContent += numArr[`${e.target.id}`]
+   }})
 
+// clear button //
+const clear = document.querySelector('#clear')
 
+clear.addEventListener('click', () => {
+    display.textContent = '';
+})
+
+const operators = document.querySelector('#operators');
+
+const opArr = {
+    plus: '+',
+    minus: '-',
+    multiply: '*',
+    divide: '/',
+}
+
+operators.addEventListener('click', (e) => {
+    if (e.target.id == 'operators' || e.target.id == 'equal') {
+        return;
+    } else {
+        display.textContent += opArr[`${e.target.id}`]
+        console.log(typeof display.textContent) 
+    };
+})
+
+const equal = document.querySelector('#equal')
+
+equal.addEventListener('click', () => {
+    let result = eval(display.textContent);
+    display.textContent = result;
+})
+
+// Operator Events //
+/*
+const plus = document.querySelector('#plus')
+const minus = document.querySelector('#minus')
+const times = document.querySelector('#multiply')
+const split = document.querySelector('#divide')
+const equal = document.querySelector('#equal')
+
+const opArr = {
+    plus: '+',
+    minus: '-',
+    multiply: '*',
+    divide: '/',
+}
+
+plus.addEventListener('click', () => {
+    let a = result + Number(display.textContent);
+    console.log(a);
+    display.textContent = '';
+    let operator = add(a,b);
+    return operator;
+});
+
+equal.addEventListener('click', () => {
+    let b = result + Number(display.textContent);
+    console.log(b);
+    return operate;
+})
+*/
