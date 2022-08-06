1ebb03e96742b0f0bfc77db60757efbf4329a15e
Paultree <paulphamily@gmail.com>
2022-08-06 15:07:19 +1000

    more additions

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 05/08/2022
 - Implemented basic calculation function.
 - Implemented basic layout of calculator.
+
+06/08/2022
+- Have implemented basic functioning of numbers appearing once you have clicked on it.
+- Bugs to be fixed: 'undefined appearing on the calculator display if div container 'numbers' is clicked or event triggered.
+- TBD: implement a feature where display content font size changes as more numbers are added. alternatively, we can try add a feature that allows the numbers to continue off but once it clips the display border, the number hides and only the most recent 10 or so numbers (maximum limit of digits allowed on display) to show.
+- TBD: Be able to do a simple calculation.
--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 // math operators //
 function add(a,b) {
-    let result = a + b;
-    return result;
+    return a += b;
 }
 
 function subtract(a,b) {
-    let result = a - b;
-    return result;
+    return a -= b;
 }
 
 function multiply(a,b) {
-    let result = a * b;
-    return result;
+    return a *=b;
 }
 
 function divide(a,b) {
-    let result = a/b;
-    return result;
+    return a /=b;
 }
 
 // simple calculation function //
 function operate(a,b,operator) {
     return operator(a,b);
 }
+
+const numArr = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+}
+
+// Simple number pressing function //
+const numbers = document.querySelector('#numbers');
+const display = document.querySelector('.calculator-display');
+
+numbers.addEventListener('click', (e) => {
+    display.textContent += numArr[`${e.target.id}`]
+   })
+
+
